28931c1b4ec6e6e74f6f2990c9bd98b02df13466
princy43 <pavani.26492@gmail.com>
2019-06-10 16:36:51 +0200

    Refactor: BowlingGame initialization moved to init method and looping of rolls moved to method rollPins in test class

--- src/test/java/com/dev/kata/BowlingGameTest.java
+++ src/test/java/com/dev/kata/BowlingGameTest.java
@@ -3,33 +3,40 @@ package com.dev.kata;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 public class BowlingGameTest {
 
+	private BowlingGame game;
+	
 	@Test
 	public void validateSetUp() {
 		assertTrue(true);
 	}
 	
+	@Before
+	public void setUp() {
+		 game = new BowlingGame();
+	}
+	
 	@Test
 	public void testGameScoreForGutterBalls() {
-		BowlingGame game= new BowlingGame();
-
-		for(int i=0;i<20;i++) {
-			game.roll(0);
-		}
+		rollPins(20,0);
 
 		assertEquals(0, game.getScore());
 	}
 	
 	@Test
 	public void testGameScoreWhenTwoPinsDownPerRoll() {
-		BowlingGame game= new BowlingGame();
-
-		for(int i=0;i<20;i++) {
-			game.roll(2);
-		}
+		rollPins(20,2);
 
 		assertEquals(40, game.getScore());
 	}
+	
+	private void rollPins(int roll,int pins) {
+		for(int index=0;index<roll;index++) {
+			game.roll(pins);
+		}
+	}
+	
 }
