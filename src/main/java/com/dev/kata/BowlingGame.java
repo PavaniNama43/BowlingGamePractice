2ca11011bce4699fade82a374a0822e42a638b93
princy43 <pavani.26492@gmail.com>
2019-06-10 17:35:52 +0200

    Added calculation of game score with one spare in rolls.

--- src/main/java/com/dev/kata/BowlingGame.java
+++ src/main/java/com/dev/kata/BowlingGame.java
@@ -1,14 +1,26 @@
 package com.dev.kata;
 
 public class BowlingGame {
-	private int score=0;
-	
+	private int rolls[]= new int[20];
+	private int currentRoll=0;
 	public void roll(int pins) {
-		score+=pins;
+		rolls[currentRoll++]=pins;
 	}
 
-	public int getScore() {
-		return score;
+	public int calculateScore() {
+		int score = 0;
+		int rollIndex = 0;
+	    for (int frame = 0; frame < 10; frame++) {
+	      if (rolls[rollIndex] + rolls[rollIndex + 1] == 10)
+	      {
+	        score += 10 + rolls[rollIndex + 2];
+	      } else {
+	        score += rolls[rollIndex] + rolls[rollIndex + 1];
+	        
+	      }
+	      rollIndex += 2;
+	    }
+	    return score;
 	}
 	
 }
--- src/test/java/com/dev/kata/BowlingGameTest.java
+++ src/test/java/com/dev/kata/BowlingGameTest.java
@@ -23,14 +23,14 @@ public class BowlingGameTest {
 	public void testGameScoreForGutterBalls() {
 		rollPins(20,0);
 
-		assertEquals(0, game.getScore());
+		assertEquals(0, game.calculateScore());
 	}
 	
 	@Test
 	public void testGameScoreWhenTwoPinsDownPerRoll() {
 		rollPins(20,2);
 
-		assertEquals(40, game.getScore());
+		assertEquals(40, game.calculateScore());
 	}
 	
 	private void rollPins(int roll,int pins) {
@@ -45,7 +45,17 @@ public class BowlingGameTest {
 		game.roll(0);
 		game.roll(0);
 		rollPins(17,1);
-		assertEquals(22, game.getScore());
+		assertEquals(22, game.calculateScore());
+	}
+	
+	@Test
+	public void testGameScoreWhenOneSpare() {
+		game.roll(2);
+		game.roll(8);
+		game.roll(7);
+		rollPins(17,2);
+		
+		assertEquals(58, game.calculateScore());
 	}
 	
 }
