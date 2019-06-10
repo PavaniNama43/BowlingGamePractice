f47b1551e1cdafede2e5f2c3f2d071d169fccf9f
princy43 <pavani.26492@gmail.com>
2019-06-10 15:41:02 +0200

    validate set up

---
+++ src/test/java/com/dev/kata/BowlingGameTest.java
@@ -0,0 +1,12 @@
+package com.dev.kata;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+public class BowlingGameTest {
+
+	@Test
+	public void validateSetUp() {
+		assertTrue(true);
+	}
+}
